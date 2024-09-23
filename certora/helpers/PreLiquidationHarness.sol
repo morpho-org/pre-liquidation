9aa7aea872ae4b7130c4d4653e0d16f7ecc1070f
Colin González <colin@morpho.xyz>
2024-09-23 16:36:04 +0200

    fix: remove natspec from harness

--- certora/helpers/PreLiquidationHarness.sol
+++ certora/helpers/PreLiquidationHarness.sol
@@ -3,10 +3,6 @@ pragma solidity 0.8.27;
 
 import {PreLiquidation, Id, MarketParams, PreLiquidationParams} from "../../src/PreLiquidation.sol";
 
-/// @title PreLiquidationHarness
-/// @author Morpho Labs
-/// @custom:contact security@morpho.org
-/// @notice The Pre Liquidation Harness Contract to be used with the Certora prover
 contract PreLiquidationHarness is PreLiquidation {
     constructor(MarketParams memory _marketParams, PreLiquidationParams memory _preLiquidationParams, address morpho)
         PreLiquidation(_marketParams, _preLiquidationParams, morpho)
