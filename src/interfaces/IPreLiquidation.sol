6de296dfde5384b860d6c5418ae58bf60ceae50a
peyha <peyha.nicole@outlook.fr>
2024-09-23 10:30:50 +0200

    doc: natspec

--- src/interfaces/IPreLiquidation.sol
+++ src/interfaces/IPreLiquidation.sol
@@ -15,19 +15,34 @@ struct PreLiquidationParams {
 /// @custom:contact security@morpho.org
 /// @notice Interface of PreLiquidation.
 interface IPreLiquidation {
+    /// @notice Morpho's address.
     function MORPHO() external view returns (IMorpho);
 
+    /// @notice The id of the Morpho Market specific to the PreLiquidation contract.
     function ID() external view returns (Id);
 
+    /// @notice The Pre LLTV of the PreLiquidation contract corresponding to
+    /// the maximum LTV of a position before allowing preliquidation.
     function PRE_LLTV() external view returns (uint256);
+    /// @notice The close factor of the PreLiquidation contract corresponding to
+    /// the maximum proportion of debt that can be pre-liquidated at once.
     function CLOSE_FACTOR() external view returns (uint256);
+    /// @notice The PreLiquidation incentive of the PreLiquidation contract corresponding to
+    /// the proportion of the liquidated debt value given to the liquidator in collateral token.
     function PRE_LIQUIDATION_INCENTIVE() external view returns (uint256);
+    /// @notice The PreLiquidation oracle of the PreLiquidation contract corresponding to
+    /// the oracle used to assess whether or not a position can be preliquidated.
     function PRE_LIQUIDATION_ORACLE() external view returns (address);
 
+    /// @notice The loan token of the Morpho Market specific to the PreLiquidation contract.
     function LOAN_TOKEN() external view returns (address);
+    /// @notice The collateral token of the Morpho Market specific to the PreLiquidation contract.
     function COLLATERAL_TOKEN() external view returns (address);
+    /// @notice The oracle address of the Morpho Market specific to the PreLiquidation contract.
     function ORACLE() external view returns (address);
+    /// @notice The IRM address of the Morpho Market specific to the PreLiquidation contract.
     function IRM() external view returns (address);
+    /// @notice The LLTV of the Morpho Market specific to the PreLiquidation contract.
     function LLTV() external view returns (uint256);
 
     /// @notice Preliquidates the given `repaidShares of debt asset or seize the given `seizedAssets`of collateral on the
