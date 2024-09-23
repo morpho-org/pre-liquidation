dfc6011ca3833446cb155651116c25cf35dd906c
peyha <peyha.nicole@outlook.fr>
2024-09-23 17:40:19 +0200

    refactor: remove constantsLib and hardcode salt

--- src/libraries/UtilsLib.sol
+++ src/libraries/UtilsLib.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import {SALT} from "../libraries/ConstantsLib.sol";
 import {PreLiquidation} from "../PreLiquidation.sol";
 import {PreLiquidationParams} from "../interfaces/IPreLiquidation.sol";
 import {Id} from "../../lib/morpho-blue/src/interfaces/IMorpho.sol";
@@ -16,6 +15,6 @@ library UtilsLib {
         bytes32 init_code_hash =
             keccak256(abi.encodePacked(type(PreLiquidation).creationCode, abi.encode(id, preLiquidationParams, morpho)));
         preLiquidationAddress =
-            address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), factory, SALT, init_code_hash)))));
+            address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), factory, uint256(0), init_code_hash)))));
     }
 }
