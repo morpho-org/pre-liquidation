92e975ea3a56fd0e82f733f3c764f9cf8fbaf0c5
peyha <peyha.nicole@outlook.fr>
2024-09-24 11:52:30 +0200

    feat: solidity version

--- src/libraries/EventsLib.sol
+++ src/libraries/EventsLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.27;
+pragma solidity ^0.8.0;
 
 import {Id, MarketParams} from "../../lib/morpho-blue/src/interfaces/IMorpho.sol";
 import {PreLiquidationParams} from "../interfaces/IPreLiquidation.sol";
--- test/PreLiquidationTest.sol
+++ test/PreLiquidationTest.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.27;
+pragma solidity ^0.8.0;
+
 
 import "../lib/forge-std/src/Test.sol";
 import "../lib/forge-std/src/console.sol";
